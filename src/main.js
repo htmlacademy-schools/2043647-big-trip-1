a529df233a0e69f3c2f8796b2d468e796faada88
Alice <alicebeilis@mail.ru>
2022-04-14 13:39:11 +0500

    add structure

--- src/main.js
+++ src/main.js
@@ -1,27 +1,34 @@
 import {createMenuTemplate} from './view/menu-view';
 import {createFilterTemplate} from './view/filter-view';
 import {createSortTemplate} from './view/sort-view';
-import {createContentTemplate} from './view/content-view';
-import {createAddNewPoint} from './view/add-new-point-view';
-import {createEditPoint} from './view/edit-point-view';
+import { createHeaderItemsTemplate } from './view/header-items-view';
+import { createAddTripEvent } from './view/add-item-event-view';
+import { createContentItemsTemplate } from './view/content-items-view';
+import { createEditTripEvent } from './view/edit-item-event-view';
+import { createContentTemplate } from './view/content-view';
 import {renderTemplate, RenderPosition} from './render.js';
+import {generatePoint} from './mock/point';
 
-const headerElement = document.querySelector('.page-header');
-const menuElement = headerElement.querySelector('.trip-controls__navigation');
+const POINT_COUNT = 10;
 
+const point = Array.from({length: POINT_COUNT}, generatePoint);
+
+const mainElement = document.querySelector('.trip-main');
+const menuElement = document.querySelector('.trip-controls__navigation')
+const filtersElement = document.querySelector('.trip-controls__filters');
+const sortElement = document.querySelector('.trip-events');
+
+renderTemplate (sortElement, createSortTemplate(), RenderPosition.BEFOREEND);
+renderTemplate (sortElement, createContentTemplate(), RenderPosition.BEFOREEND);
 renderTemplate (menuElement, createMenuTemplate(), RenderPosition.BEFOREEND);
-
-const filtersElement = headerElement.querySelector('.trip-controls__filters');
-
 renderTemplate (filtersElement, createFilterTemplate(), RenderPosition.BEFOREEND);
+renderTemplate (mainElement, createHeaderItemsTemplate(point[0]), RenderPosition.AFTERBEGIN);
 
-const mainElement = document.querySelector('.page-main');
-const mainContentElement = mainElement.querySelector('.page-body__container');
+const eventElement = sortElement.querySelector('.trip-events__list');
 
-renderTemplate (mainContentElement, createSortTemplate(), RenderPosition.BEFOREEND);
-renderTemplate (mainContentElement, createContentTemplate(), RenderPosition.BEFOREEND);
+renderTemplate (eventElement, createAddTripEvent(point[0]), RenderPosition.BEFOREEND);
+renderTemplate (eventElement, createEditTripEvent(point[0]), RenderPosition.BEFOREEND);
 
-const eventsElement = document.querySelector('.trip-events__list');
-
-renderTemplate (eventsElement, createEditPoint(), RenderPosition.AFTERBEGIN);
-renderTemplate (eventsElement, createAddNewPoint(), RenderPosition.AFTERBEGIN);
+for (let i = 1; i < POINT_COUNT; i++) {
+    renderTemplate(eventElement, createContentItemsTemplate(point[i]), RenderPosition.BEFOREEND);
+}
