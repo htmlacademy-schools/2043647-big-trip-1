359dbcc1f00e7c6c9941dfd928d3993f8be3b91e
Alice <alicebeilis@mail.ru>
2022-04-25 18:04:44 +0500

    add empty list

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import AddNewPointView from './view/add-item-event-view';
 import ContentItemView from './view/content-items-view';
 import EditPointView from './view/edit-item-event-view';
 import ContentListView from './view/content-list-view';
+import EmptyListView from './view/empty-list-view';
 import {render, RenderPosition} from './render.js';
 import {generatePoint} from './mock/point';
 
@@ -19,10 +20,7 @@ const filtersElement = document.querySelector('.trip-controls__filters');
 const sortElement = document.querySelector('.trip-events');
 const contentListElement = new ContentListView();
 
-render (sortElement, new SortView().element, RenderPosition.BEFOREEND);
-render (sortElement, contentListElement.element, RenderPosition.BEFOREEND);
-render (menuElement, new MenuView().element, RenderPosition.BEFOREEND);
-render (filtersElement, new FilterView().element, RenderPosition.BEFOREEND);
+
 render (mainElement, new HeaderView(point).element, RenderPosition.AFTERBEGIN);
 render (contentListElement.element, new AddNewPointView(point[0]).element, RenderPosition.BEFOREEND);
 
@@ -37,18 +35,42 @@ const renderEvent = (eventListElement, event) => {
     eventListElement.replaceChild(editComponent.element, itemComponent.element);
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Esc' || evt.key === 'Escape') {
+      evt.preventDefault();
+      replaceFormToItem();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   itemComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
     replaceItemToForm();
+    document.addEventListener('keydown', onEscKeyDown);
   });
-
+  editComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceFormToItem();
+  });
   editComponent.element.querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormToItem();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   render(eventListElement, itemComponent.element, RenderPosition.BEFOREEND);
 };
 
-for (let i = 1; i < POINT_COUNT; i++) {
-  renderEvent(contentListElement.element, point[i]);
+if (point.length === 0){
+  render (menuElement, new MenuView().element, RenderPosition.BEFOREEND);
+  render (filtersElement, new FilterView().element, RenderPosition.BEFOREEND);
+  render(sortElement, new EmptyListView.element, RenderPosition.BEFOREEND);
+}
+else {
+  render (sortElement, contentListElement.element, RenderPosition.BEFOREEND);
+  render (menuElement, new MenuView().element, RenderPosition.BEFOREEND);
+  render (filtersElement, new FilterView().element, RenderPosition.BEFOREEND);
+  render (sortElement, new SortView().element, RenderPosition.AFTERBEGIN);
+
+  for (let i = 1; i < POINT_COUNT; i++) {
+    renderEvent(contentListElement.element, point[i]);
+  }
 }
